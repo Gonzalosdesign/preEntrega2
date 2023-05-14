9202778581aa3aba7a217d77d35c663a3bd5bd2f
Gonzalo Souto <gonzalosdesign@gmail.com>
2023-05-14 18:44:13 -0300

    versión inicial, array de objetos: ingresos y egresos, promt y consola

---
+++ js/byDom.js
@@ -0,0 +1,41 @@
+/* PROGRAMA
+
+- agregar objetos
+- eliminar objetos
+- imprimir objetos
+- buscar objetos (buscar movimientos)
+- sumar objetos (mostrar saldo)
+- 
+*/
+
+let saldo = 0;
+
+function Movimientos (importe, descripcion, fecha){
+    this.importe = importe;
+    this.descripcion = descripcion;
+    this.fecha = fecha;
+}
+
+let ingresos = document.getElementById("ingresos");
+    ingresos.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    let inputs = e.target.children;
+    console.log(inputs[1].value);
+
+    let Movimiento = new Movimientos (inputs[0].value, inputs[1].value, new Date());
+    console.log(Movimiento.importe);
+    
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <h2>Importe: ${Movimiento.importe}</h2>
+    <h5>Descripción: ${Movimiento.descripcion}</h5>
+    <h6>Fecha: ${Movimiento.fecha}</h6>
+    `;
+    document.body.append(div);
+
+    saldo = (saldo + Movimiento.importe);
+});
+
+let saldoDisplay = document.getElementById("saldo");
+    saldoDisplay.innerHTML = `<h3>Saldo: ${saldo}</h3>`;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,57 @@
 /* PROGRAMA
-
-- agregar objetos
-- eliminar objetos
-- imprimir objetos
-- buscar objetos
-- sumar objetos
-- 
+// - agregar objetos
+- imprimir objetos en el DOM
+- buscar objetos (buscar movimientos)
+// - sumar objetos (mostrar saldo)
 */
+
+
+let saldo = 0;
+console.log(`Saldo inicial: ${saldo}`);
+const ingresos = [];
+const egresos = [];
+
+alert("Detalle sus ingresos y egresos. Utilice la consola para ver su saldo y busque movimientos dentro de un rango de importes.");
+
+function Ingresos (importe, descripcion, fecha){
+    this.importe = importe;
+    this.descripcion = descripcion;
+    this.fecha = fecha;
+}
+
+function Egresos (importe, descripcion, fecha){
+    this.importe = importe;
+    this.descripcion = descripcion;
+    this.fecha = fecha;
+}
+
+function ingresarIngresos () {
+    let ingreso = new Ingresos (parseFloat(prompt("Importe")), prompt("Detalle"), new Date());
+    saldo = saldo+ingreso.importe;
+    console.log(`Importe: ${ingreso.importe}, descripción: ${ingreso.descripcion}, Fecha: ${ingreso.fecha}`);
+    console.log(`Saldo luego del ingreso: ${saldo}`);
+    ingresos.push(ingreso);
+}
+
+function ingresarEgresos (){
+    let egreso = new Egresos (parseFloat(prompt("Importe")), prompt("Detalle"), new Date());
+    saldo = saldo-egreso.importe;
+    console.log(`Importe: ${egreso.importe}, descripción: ${egreso.descripcion}, Fecha: ${egreso.fecha}`);
+    console.log(`Saldo luego del egreso: ${saldo}`);
+    egresos.push(egreso);
+}
+
+let cantidadIngresos = parseInt(prompt("Detalle la cantidad de ingresos totales"));
+for (i = 0; i < cantidadIngresos; i++){
+    ingresarIngresos();
+}
+let cantidadEgresos = parseInt(prompt("Detalle la cantidad de egresos totales"));
+for (i = 0; i < cantidadEgresos; i++){
+    ingresarEgresos();
+}
+console.log(`Arreglo con todos los ingresos: ${ingresos}`);
+console.log(`Arreglo con todos los egresos: ${egresos}`);
+
+
+// let minBuscado = parseFloat(prompt("ingrese el mínimo buscado"));
+// let maxBuscado = parseFloat(prompt("ingrese el máximo buscado"));
