1e011b9641f4a268c1886b06af79eb2807efd319
Gonzalo Souto <gonzalosdesign@gmail.com>
2023-05-27 17:02:56 -0300

    vuelve a la versión entregada para el curso

--- js/saldo.js
+++
@@ -1,7 +0,0 @@
-let saldo = 0;
-
-function mostrarSaldo() {
-    console.log(saldo);
-    let saldoDisplay = document.getElementById("saldo");
-    saldoDisplay.innerHTML = `<h3>Su saldo actual es de: ${saldo}</h3>`;
-};
